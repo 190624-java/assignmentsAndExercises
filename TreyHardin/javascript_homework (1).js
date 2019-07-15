7971acccacc4639b0f915a12f650585bab99495d
Marshall-Hardin <52173070+Marshall-Hardin@users.noreply.github.com>
2019-07-15 16:59:16 -0400

    Add files via upload

---
+++ TreyHardin/javascript_homework (1).js
@@ -0,0 +1,111 @@
+var homework = {};
+module.exports = homework;
+
+/*
+  1. Return the nth fibonacci number
+
+  f(0) = 0
+  f(1) = 1
+  f(10) = 55
+*/
+homework.fibonacci = function(n){
+
+  var a = 1, b = 0, temp;
+
+  while (n >= 0){
+    temp = a;
+    a = a + b;
+    b = temp;
+    n--;
+  }
+
+  return b;
+
+};
+
+/*
+  2. Sort array of integers
+
+  f([2,4,5,1,3,1]) = [1,1,2,3,4,5]
+
+  Don't use the Array sort() method... that would be lame.
+*/
+homework.sort = function(array) {
+
+  let len = array.length();
+  for(let i = 0; i < len - 1; i++){
+    for(let j = 0; j < len-i-1; j++){
+      let temp = arr[j]; 
+      array[j] = array[j+1]; 
+      array[j+1] = temp; 
+    }
+  }
+
+  return array;
+};
+
+/*
+  3. Return the factorial of n
+
+  f(0) = 1
+  f(1) = 1
+  f(3) = 6
+*/
+homework.factorial = function(n){
+
+  let total = 1;
+
+  if(n == 0){
+    return 1;
+  }
+  else{
+    for(let i = 1; i <= n; i++){
+      total = total * i;
+    }
+  }
+  return total;
+};
+
+/*
+  4. Rotate left
+
+  Given array, rotate left n times and return array
+
+  f([1,2,3,4,5], 1) = [2,3,4,5,1]
+  f([1,2,3,4,5], 6) = [2,3,4,5,1]
+  f([1,2,3,4,5], 3) = [4,5,1,2,3]
+
+*/
+homework.rotateLeft = function(array, n) {
+
+  for(let i = 0; i < n; i++){
+    array = array.shift();
+  }
+
+  return array;
+
+};
+
+/*
+  Bonus: Balanced Brackets
+
+  A bracket is any one of the following: (, ), {, }, [, or ]
+
+  The following are balanced brackets:
+    ()
+    ()()
+    (())
+    ({[]})
+
+  The following are NOT balanced brackets:
+  (
+  )
+  (()
+  ([)]
+
+  Return true if balanced
+  Return false if not balanced
+*/
+homework.balancedBrackets = function(bracketsString){
+
+};
